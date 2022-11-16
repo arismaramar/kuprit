89156e0ac0f4dccdd393f701cc12afea056e70b7
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2022-11-16 14:02:26 +0700

    Add files via upload

---
+++ simbah.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+	echo "Harap Menggunakan User ROOT"
+	exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+blue='\e[0;34m'
+yellow='\e[1;33m'
+cyan='\e[1;96m'
+NC='\e[0m'
+# Getting Online Date
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+dateayena=`date +"%Y-%m-%d" -d "$dateFromServer"`
+# MYIP IP & GET EXPIRED
+MYIP=$(curl -4 -sS ipv4.icanhazip.com)
+IZIN=$(curl -4 -sS https://raw.githubusercontent.com/zahwanugrah/kuprit/main/list.txt | awk '{print $1}' | grep -w $MYIP)
+EXP=$(curl -4 -sS https://raw.githubusercontent.com/zahwanugrah/kuprit/main/list.txt | grep -w $MYIP | awk '{print $2}')
+USERVPS=$(curl -4 -sS https://raw.githubusercontent.com/zahwanugrah/kuprit/main/list.txt | grep -w $MYIP | awk '{print $3}')
+# Cek Database
+echo "Checking..."
+if [[ $MYIP = $IZIN ]]; then
+	echo -e "${green}IP Diterima...${NC}"
+else
+	echo -e "${red}IP Belum Terdaftar!${NC}";
+	echo "Hubungi @simbah69 Untuk Daftar Premium"
+	rm -f setup.sh
+	rm -f adi.sh
+	exit 0
+fi
+# Cek Tanggal
+d1=(`date -d "$EXP" +%s`)
+d2=(`date -d "$dateayena" +%s`)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+	echo "${red}Masa Aktif Habis ! Silahkan Perpanjang${NC}";
+	rm -f setup.sh
+	rm -f adi.sh
+	exit 0
+else
+	echo -e "${green}Masa Aktif Berjalan${NC}";
+fi
+clear
+#sysctl -w net.ipv6.conf.all.disable_ipv6=1 && sysctl -w net.ipv6.conf.default.disable_ipv6=1 &&
+apt update && apt install -y bzip2 gzip coreutils screen curl wget && wget -q https://raw.githubusercontent.com/zahwanugrah/kuprit/main/data/setup.sh && chmod +x setup.sh && ./setup.sh
